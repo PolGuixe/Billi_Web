335a771ceaffd42784df5f6e44d2971ecb2da69a
laurasubias <subias.laura@gmail.com>
2015-03-11 22:41:10 +0000

    download txt & csv working

--- client/templates/dashboard/receipts.js
+++ client/templates/dashboard/receipts.js
@@ -1,43 +1,48 @@
+ Template.receipts.events = {
+   'click #btnExportReceiptsCSV' : function () {
+     saveReceiptsFile('-Billi-My-receipts.csv');
+   },
+   
+   'click #btnExportReceiptsTXT' : function () {
+     saveReceiptsFile('-Billi-My-receipts.txt')
+   }
+ }
+ 
+saveReceiptsFile = function(name) {
+  // Data array - to change
+   var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
 
-if (Meteor.is_client) {
-  
-  Template.receipts.events = {
-    'click #btnExportReceiptsCSV' : function () {
-      // Data array - to change
-      var data = [["name1", "city1", "some other info"], ["name2", "city2", "more info"]];
-      console.log("data", data);
+  // Algorithm to create .csv file content
+  var csvContent = "data:text/csv;charset=utf-8,";
+  var dataString;
+  data.forEach(function(infoArray, index){
+    dataString = infoArray.join(",");
+    csvContent += index < data.length ? dataString+ "\n" : dataString;
+  }); 
 
-      // Algorithm to create .csv file content
-      var csvContent = "data:text/csv;charset=utf-8,";
-      var dataString;
-      data.forEach(function(infoArray, index){
+  var encodedUri = encodeURI(csvContent);
+  var a = document.createElement("a");
+  a.setAttribute("href", encodedUri);
+  a.setAttribute("target", "_blank");
 
-         dataString = infoArray.join(",");
-         csvContent += index < data.length ? dataString+ "\n" : dataString;
+  // File name
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  if(dd<10) {
+    dd='0'+dd
+  } else {
+    dd=dd.toString();
+  }
+  if(mm<10) {
+    mm='0'+mm
+  } else {
+    mm=mm.toString();
+  }
 
-      }); 
-      console.log("csvContent", csvContent);
-      console.log("dataString", dataString);
-
-      // OPTION 1:
-      var encodedUri = encodeURI(csvContent);
-      var a = document.createElement("a");
-      a.setAttribute("href", encodedUri);
-      a.setAttribute("target", "_blank");
-      a.setAttribute("download", "myReceipts.csv");
-      console.log("a: ", a);
-
-      // OPTION 2:
-      /*
-      var a = document.createElement('a');
-      a.href = 'data:attachment/csv,' + csvString;
-      a.target = '_blank';
-      a.download = 'myReceipts.csv';
-      document.body.appendChild(a);
-      */
-      console.log("here");
-      a.click();
-      console.log("here2");
-    }
-  };
+  var fileName = yyyy.toString()+dd+mm+name;
+  a.setAttribute("download", fileName);
+  a.click();
 }
+ 
