da9f8079fc637342894f93b086ce0f7d9984a4c5
PolGuixe <pol.guixe@gmail.com>
2015-04-06 17:30:18 +0200

    Modified style and html for the landing page

---
+++ client/templates/landing_page/landing_page_nav.js
@@ -0,0 +1,12 @@
+Template.landingPageNav.rendered = function() {
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    // TODO: move this to meteor style
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+};
